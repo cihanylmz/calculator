bdf03e3dd03c0488fdf10a3e14fa3be7f0f9f1ca
Cihan <ylmz_cihan@outlook.com>
2022-03-07 14:09:54 +0300

    Made operations functionality with buttons

--- script.js
+++ script.js
@@ -14,12 +14,46 @@ const operate = function(operandA, operandB, operator) {
 }
 
 
-//detect click on each number
-let display = document.querySelector('div');
+//make the display show numbers clicked
+const displayNums = function(digit) {
+    display.textContent += digit;
+    currentNum = +display.textContent;
+}
+
+
+//when an operator is clicked save the operator and operand and clear the display
+const operatorClick = function(operator) {
+    nextNum = currentNum;
+    oprtr = operator;
+    display.textContent = "";
+}
+//when equal sign is clicked, make the operations using operands and the operator.
+const equalClick = function(operandA, ) {
+    display.textContent = operate(currentNum, nextNum, oprtr);
+    currentNum = +display.textContent;
+}
+
+const display = document.querySelector('div');
 const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+
+var currentNum = 0;
+var nextNum = 0;
 numbers.forEach(number => number.addEventListener(
-    'click', () => display.textContent += number.textContent));
-const clearBtn = document.querySelector('#clear');
-clearBtn.addEventListener('click', () => display.textContent = "");
-//make that number go to the div
-//store the number on the div
+    'click', () => displayNums(number.textContent)
+    ));
+
+var oprtr = '';
+operators.forEach(operator => operator.addEventListener(
+    'click', () => operatorClick(operator.textContent))
+    );
+
+const equalBtn = document.querySelector('#equals');
+equalBtn.addEventListener('click', () => {
+    equalClick();
+});
+
+
+
+//const clearBtn = document.querySelector('#clear');
+//clearBtn.addEventListener('click', () => display.textContent = "");
